610b7d9adf1425e68d9ef40f38bb7196dd7aa31c
jmessina <j.w.messina24@gmail.com>
2023-08-20 16:24:54 -0600

    Uploaded files from local repo manually
    because GitHuh's UI is ABSOLUTE TRASH

---
+++ PracticeApp1/PracticeApp1/PracticeApp1App.swift
@@ -0,0 +1,14 @@
+// Hopefully GitHub and it's stupidly user-unfriendly design allows me to upload this project
+// now that I've gone and typed this
+
+
+import SwiftUI
+
+@main
+struct PracticeApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
